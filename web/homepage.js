7a3c22227da45667ec3a823de7231136e0ad2638
amartherus <andrew@martherus.com>
2017-09-23 15:08:33 -0600

    added css and js

--- web/homepage.js
+++ web/homepage.js
@@ -1,6 +1,6 @@
 function reveal() {
   //going to reveal the image when clicked
-  document.getElementById('myPhoto').style.display = 'inline-block';
+  document.getElementById('myPhoto').style.display = 'block';
   document.getElementById('netflix').style.display = 'inline-block';
   document.getElementById('xbox').style.display = 'inline-block';
   document.getElementById('hiking').style.display = 'inline-block';

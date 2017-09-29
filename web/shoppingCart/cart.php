5af49151e6edd347151b2b66359a415fa82a64ff
amartherus <andrew@martherus.com>
2017-09-29 11:32:47 -0600

    initial shoppingCart commit

--- web/shoppingCart/cart.php
+++ web/shoppingCart/cart.php
@@ -9,7 +9,7 @@
 
     <ul class="list-group">
       <li class="list-group-item"><img src="../photos/spongebob.png" alt="Spongebob" height="200" width="100">
-        <?php echo $_POST["spongebob_quantity"]; ?>
+      </br><?php echo $_POST["spongebob_quantity"]; ?>
       <li class="list-group-item"><img src="../photos/patrick.png" alt="Patrick" height="200" width="100">
       <li class="list-group-item"><img src="../photos/Squidward.png" alt="Squidward" height="200" width="100">
       <li class="list-group-item"><img src="../photos/gary_the_snail.png" alt="Gary the Snail" height="200" width="100">

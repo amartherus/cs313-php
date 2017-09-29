5b1e6c805d4ab9e790407628bed1452efdf2b95f
amartherus <andrew@martherus.com>
2017-09-29 11:23:40 -0600

    initial shoppingCart commit

---
+++ web/shoppingCart/cart.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Andrew Martherus' Assignments Page</title>
+    <link rel="stylesheet" href="shoppingcart.css">
+    <script src="shoppingcart.js"></script>
+  </head>
+  <body>
+
+    <ul class="list-group">
+      <li class="list-group-item"><img src="../photos/spongebob.png" alt="Spongebob" height="200" width="100">
+        <?php echo $_POST["spongebob_quantity"]; ?>
+      <li class="list-group-item"><img src="../photos/patrick.png" alt="Patrick" height="200" width="100">
+      <li class="list-group-item"><img src="../photos/Squidward.png" alt="Squidward" height="200" width="100">
+      <li class="list-group-item"><img src="../photos/gary_the_snail.png" alt="Gary the Snail" height="200" width="100">
+      <li class="list-group-item"><img src="../photos/Mr_Krabs.png" alt="Mr. Krabs" height="200" width="100">
+    </ul>
+
+  </body>
+</html>

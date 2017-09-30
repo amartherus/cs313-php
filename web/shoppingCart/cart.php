bcd32c8075000c927f816b6f4825d1ebb6a105cd
amartherus <andrew@martherus.com>
2017-09-30 17:01:09 -0600

    adding session variables

--- web/shoppingCart/cart.php
+++ web/shoppingCart/cart.php
@@ -15,19 +15,19 @@ session_start();
     <ul class="list-group">
       <li class="list-group-item"><img src="../photos/spongebob.png" alt="Spongebob" height="200" width="100">
       </br><?php echo "$".$_SESSION["spongebob_price"];?>
-    </br><?php echo "Quantity: ".$_SESSION["spongebob_quantity"];?>
+    </br><?php echo "Quantity: ".$_POST["spongebob_quantity"];?>
       <li class="list-group-item"><img src="../photos/Patrick.png" alt="Patrick" height="200" width="100">
       </br><?php echo "$".$_SESSION["patrick_price"];?>
-    </br><?php echo "Quantity: ".$_SESSION["patrick_quantity"];?>
+    </br><?php echo "Quantity: ".$_POST["patrick_quantity"];?>
       <li class="list-group-item"><img src="../photos/Squidward.png" alt="Squidward" height="200" width="100">
       </br><?php echo "$".$_SESSION["squidward_price"];?>
-    </br><?php echo "Quantity: ".$_SESSION["squidward_quantity"];?>
+    </br><?php echo "Quantity: ".$_POST["squidward_quantity"];?>
       <li class="list-group-item"><img src="../photos/gary_the_snail.png" alt="Gary the Snail" height="200" width="100">
       </br><?php echo "$".$_SESSION["gary_price"];?>
-    </br><?php echo "Quantity: ".$_SESSION["gary_quantity"];?>
+    </br><?php echo "Quantity: ".$_POST["gary_quantity"];?>
       <li class="list-group-item"><img src="../photos/Mr_Krabs.png" alt="Mr. Krabs" height="200" width="100">
       </br><?php echo "$".$_SESSION["krab_price"];?>
-    </br><?php echo "Quantity: ".$_SESSION["krab_quantity"];?>
+    </br><?php echo "Quantity: ".$_POST["krab_quantity"];?>
     </ul>
 
     <button Onclick="window.location.href='browseItems.php'"type="button" class="btn">Go Back to Browsing</button>
@@ -40,7 +40,7 @@ session_start();
     $_SESSION["gary_quantity"] = $_POST["gary_quantity"];
     $_SESSION["krab_quantity"] = $_POST["krab_quantity"];
     ?>
-    
+
     <input type="submit" value="Click to Check Out">
   </form>
 

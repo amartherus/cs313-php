998e5046f1eac363c5f6e2508d50b607f40d777b
amartherus <andrew@martherus.com>
2017-09-30 17:06:56 -0600

    adding session variables

--- web/shoppingCart/checkout.php
+++ web/shoppingCart/checkout.php
@@ -16,19 +16,19 @@ Does this appear correct?</br>
         <ul class="list-group">
           <li class="list-group-item"><img src="../photos/spongebob.png" alt="Spongebob" height="200" width="100">
           </br><?php echo "$".$_SESSION["spongebob_price"];?>
-        </br><?php echo "Quantity: ".$_POST["spongebob_quantity"];?>
+        </br><?php echo "Quantity: ".$_SESSION["spongebob_quantity"];?>
           <li class="list-group-item"><img src="../photos/Patrick.png" alt="Patrick" height="200" width="100">
           </br><?php echo "$".$_SESSION["patrick_price"];?>
-        </br><?php echo "Quantity: ".$_POST["patrick_quantity"];?>
+        </br><?php echo "Quantity: ".$_SESSION["patrick_quantity"];?>
           <li class="list-group-item"><img src="../photos/Squidward.png" alt="Squidward" height="200" width="100">
           </br><?php echo "$".$_SESSION["squidward_price"];?>
-        </br><?php echo "Quantity: ".$_POST["squidward_quantity"];?>
+        </br><?php echo "Quantity: ".$_SESSION["squidward_quantity"];?>
           <li class="list-group-item"><img src="../photos/gary_the_snail.png" alt="Gary the Snail" height="200" width="100">
           </br><?php echo "$".$_SESSION["gary_price"];?>
-        </br><?php echo "Quantity: ".$_POST["gary_quantity"];?>
+        </br><?php echo "Quantity: ".$_SESSION["gary_quantity"];?>
           <li class="list-group-item"><img src="../photos/Mr_Krabs.png" alt="Mr. Krabs" height="200" width="100">
           </br><?php echo "$".$_SESSION["krab_price"];?>
-        </br><?php echo "Quantity: ".$_POST["krab_quantity"];?>
+        </br><?php echo "Quantity: ".$_SESSION["krab_quantity"];?>
         </ul>
 
         <p>Please enter your address:</p></br>
@@ -37,7 +37,7 @@ Does this appear correct?</br>
           <li class="list-group-item">City: <input type="text" name="city"></br>
           <li class="list-group-item">State: <input type="text" name="state"></br>
           <li class="list-group-item">Zip: <input type="text" name="zip"></br>
-            
+
         </ul>
 
 

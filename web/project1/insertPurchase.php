1922735e11e8680ba66a71e05da2ae4805a41d16
amartherus <andrew@martherus.com>
2017-10-25 09:15:20 -0600

    using instructor solutions as reference to my insert problem

--- web/project1/insertPurchase.php
+++ web/project1/insertPurchase.php
@@ -35,7 +35,7 @@ try {
 
   //this is now the customerid of a given username
   $customerid = $statement->fetch();
-  $customerid = $customerid['name'];
+  //$customerid = $customerid['name'];
   echo "customerid: ".$customerid."</br>";
 
   $insertPurchase = 'INSERT INTO purchase(customerid, totalamount)
